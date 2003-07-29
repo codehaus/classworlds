77a2bf2ebef333e0fbdaf52e4163ebb1acb0ea84
nobody <nobody@10ba983f-581b-0410-a3e8-de4f8b17c73e>
2003-07-29 04:37:59 +0000

    This commit was manufactured by cvs2svn to create branch 'CODEHAUS'.

---
+++ branches/CODEHAUS/classworlds-1.0/src/java/test/org/codehaus/classworlds/EntryTest.java
@@ -0,0 +1,48 @@
+package org.codehaus.classworlds;
+
+import junit.framework.TestCase;
+
+/**
+ * @author <a href="bwalding@jakarta.org">Ben Walding</a>
+ * @version $Id$
+ */
+public class EntryTest extends TestCase
+{
+
+    /**
+     * Constructor for EntryTest.
+     * @param arg0
+     */
+    public EntryTest(String name)
+    {
+        super(name);
+    }
+
+    public void testCompareTo() throws Exception
+    {
+        ClassWorld cw = new ClassWorld();
+        ClassRealmImpl r = (ClassRealmImpl) cw.newRealm("test1");
+
+        Entry entry1 = new Entry(r, "org.test");
+        Entry entry2 = new Entry(r, "org.test.impl");
+
+        assertTrue("org.test > org.test.impl", entry1.compareTo(entry2) > 0);
+    }
+
+    /**
+     * Tests the equality is realm independant
+     * @throws Exception
+     */
+    public void testEquals() throws Exception
+    {
+        ClassWorld cw = new ClassWorld();
+        ClassRealmImpl r1 = (ClassRealmImpl) cw.newRealm("test1");
+        ClassRealmImpl r2 = (ClassRealmImpl) cw.newRealm("test2");
+
+        Entry entry1 = new Entry(r1, "org.test");
+        Entry entry2 = new Entry(r2, "org.test");
+
+        assertTrue("entry1 == entry2", entry1.equals(entry2));
+    }
+
+}
