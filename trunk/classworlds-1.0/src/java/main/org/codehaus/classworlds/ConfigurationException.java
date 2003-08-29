de332e0298ade200efa9db0594f7b5301a91b244
user57 <user57@10ba983f-581b-0410-a3e8-de4f8b17c73e>
2003-08-29 08:41:40 +0000

    o Added constructor to handle exception message formatting

--- trunk/classworlds-1.0/src/java/main/org/codehaus/classworlds/ConfigurationException.java
+++ trunk/classworlds-1.0/src/java/main/org/codehaus/classworlds/ConfigurationException.java
@@ -62,4 +62,15 @@ public class ConfigurationException extends Exception
     {
         super( msg );
     }
+    
+    /** Construct.
+     *
+     *  @param msg The message.
+     *  @param lineNo The number of configuraton line where the problem occured.
+     *  @param line The configuration line where the problem occured.
+     */
+    public ConfigurationException(String msg, int lineNo, String line)
+    {
+        super( msg + " (" + lineNo + "): " + line );
+    }
 }
