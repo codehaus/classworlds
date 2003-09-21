2c63f7a1825399e8b17bc7efe2492595f72bf6c3
jvanzyl <jvanzyl@10ba983f-581b-0410-a3e8-de4f8b17c73e>
2003-09-21 23:30:10 +0000

    o Adding some goodies for resource retrieval testing. o Adding a Util jar for normalizing urls as it appears to be requried   in two separate places. In the ClassRealm itself and in our implementation   of the JarUrlConnection.

--- trunk/classworlds-1.0/src/java/test/org/codehaus/classworlds/ResourcesTest.java
+++ trunk/classworlds-1.0/src/java/test/org/codehaus/classworlds/ResourcesTest.java
@@ -4,7 +4,7 @@ package org.codehaus.classworlds;
  $Id$
 
  Copyright 2002 (C) The Werken Company. All Rights Reserved.
- 
+
  Redistribution and use of this software and associated documentation
  ("Software"), with or without modification, are permitted provided
  that the following conditions are met:
@@ -12,25 +12,25 @@ package org.codehaus.classworlds;
  1. Redistributions of source code must retain copyright
     statements and notices.  Redistributions must also contain a
     copy of this document.
- 
+
  2. Redistributions in binary form must reproduce the
     above copyright notice, this list of conditions and the
     following disclaimer in the documentation and/or other
     materials provided with the distribution.
- 
+
  3. The name "classworlds" must not be used to endorse or promote
     products derived from this Software without prior written
     permission of The Werken Company.  For written permission,
     please contact bob@werken.com.
- 
+
  4. Products derived from this Software may not be called "classworlds"
     nor may "classworlds" appear in their names without prior written
     permission of The Werken Company. "classworlds" is a registered
     trademark of The Werken Company.
- 
+
  5. Due credit should be given to The Werken Company.
     (http://classworlds.werken.com/).
- 
+
  THIS SOFTWARE IS PROVIDED BY THE WERKEN COMPANY AND CONTRIBUTORS
  ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
  NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
@@ -43,7 +43,7 @@ package org.codehaus.classworlds;
  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  */
 
 import junit.framework.TestCase;
@@ -79,18 +79,18 @@ public class ResourcesTest
     public void testConfigure_Inheritance() throws Exception
     {
         this.configurator.configure( getConfigPath( "classworlds.conf" ) );
-        
+
         ClassWorld classWorld = launcher.getWorld();
 
         // Grab our root realm.
         ClassRealm rootMavenPlugin = classWorld.getRealm( "root" );
-        
+
         assertNotNull( rootMavenPlugin.getResource( "org/codehaus/werkflow/personality/common/core-idioms.xml" ) );
 
         assertNotNull( rootMavenPlugin.getResource( "/org/codehaus/werkflow/personality/common/core-idioms.xml" ) );
 
-        //assertNotNull( "Double dot path element is not working!",
-        //               rootMavenPlugin.getResource( "org/codehaus/werkflow/personality/basic/../common/core-idioms.xml" ) );
+        assertNotNull( "Double dot path element is not working!",
+                       rootMavenPlugin.getResource( "org/codehaus/werkflow/personality/basic/../common/core-idioms.xml" ) );
     }
 
     private FileInputStream getConfigPath(String name)
