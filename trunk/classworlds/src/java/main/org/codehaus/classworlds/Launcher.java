1019e7256b6a9b0d5c2488cde6e8e8982319bf5f
dandiep <dandiep@10ba983f-581b-0410-a3e8-de4f8b17c73e>
2004-09-19 17:21:07 +0000

    Don't System.exit(), which will kill threads wich aren't finished yet. Instead, just return the return code.

--- trunk/classworlds/src/java/main/org/codehaus/classworlds/Launcher.java
+++ trunk/classworlds/src/java/main/org/codehaus/classworlds/Launcher.java
@@ -351,22 +351,22 @@ public class Launcher
 
     /**
      * Launch the launcher from the command line.
-     * Will exit using System.exit with an exit code of 0 for success, 100 if there was an unknown exception,
+     * Will exit using an exit code of 0 for success, 
+     * 100 if there was an unknown exception,
      * or some other code for an application error.
      *
      * @param args The application command-line arguments.
      */
-    public static void main( String[] args )
+    public static int main( String[] args )
     {
         try
         {
-            int exitCode = mainWithExitCode( args );
-            System.exit( exitCode );
+            return mainWithExitCode( args );
         }
         catch ( Exception e )
         {
             e.printStackTrace();
-            System.exit( 100 );
+            return 100;
         }
     }
 
